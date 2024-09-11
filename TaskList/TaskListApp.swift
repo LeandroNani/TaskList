b5d417019382084e7832f6e8262c826a9ff688de
Leandro Pacheco <leandropacheco02@hotmail.com>
2024-09-11 14:45:28 -0300

    Initial Commit

---
+++ TaskList/TaskListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskListApp.swift
+//  TaskList
+//
+//  Created by dtidigital on 11/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct TaskListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
